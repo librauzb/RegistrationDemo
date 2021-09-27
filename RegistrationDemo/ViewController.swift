74a5caceb3eaefdf37c198ae6226a5443a03457e
Ulmasbek <uakhtamov@gmail.com>
2021-09-27 08:48:23 +0500

    Initial Commit

---
+++ RegistrationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationDemo
+//
+//  Created by Ulmasbek on 2021/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
